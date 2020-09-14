7be2f6f08ee08e767e1cfdbe760ec375ef716a38
ICEEDev <68020557+ICEEDev@users.noreply.github.com>
2020-09-14 18:52:07 -0500

    Create PF Auto Deploy.lua

---
+++ PF Auto Deploy.lua
@@ -0,0 +1,17 @@
+local _deploy;
+
+for i,v in next, getgc() do
+    if (typeof(v) == "function") then
+        for a, b in next, debug.getupvalues(v) do
+            if (typeof(b) == "table" and rawget(b, "deploy")) then
+                _deploy = b;
+            end;
+        end;
+    end;
+
+    if (_deploy) then
+        break;
+    end;
+end;
+
+_deploy:deploy()
