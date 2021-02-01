f3831b336a94c67a2339f64097180fde50cc5750
ICEEDev <68020557+ICEEDev@users.noreply.github.com>
2021-01-31 22:46:59 -0600

    Update RogueAntiFire.lua

--- RogueAntiFire.lua
+++ RogueAntiFire.lua
@@ -16,11 +16,11 @@ end;
 
 -- ChildAdded
 if (Player.Character) then
-    Player.Character.ChildAdded:connect(function(Child)
+    Player.Character.ChildAdded:Connect(function(Child)
         if (tostring(Child) == "Burning" and Player.Character and Player.Character:FindFirstChild("CharacterHandler")) then
             -- For i,v loop on all remotes
             for i,v in next, Player.Character.CharacterHandler.Remotes:GetChildren() do
-                if (v.Name:len() == 36 or v.Name == "Dodge") then
+                if (tostring(v):len() == 36 or tostring(v) == "Dodge") then
                     -- fires all remotes ( ik aids but its a meme method i found )
                     v:FireServer({math.random(1, 4), 0});
                     if (not Player.Character:FindFirstChild("Burning")) then break; end;
